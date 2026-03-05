d8272fc4c458d5441b8fc1ef07852514f2ca12db
Vico <gino.vico@cevalogistics.com>
2026-03-05 08:37:18 -0300

    Trigger: Build #20 with fixed assets and workflow

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from datetime import datetime
 import requests
 
 # --- CONFIGURACIÓN ---
+# Update: Triggering build #20
 DB_NAME = "datos_logistica.db"
 USUARIOS_NAME = "Usuarios.xlsx"
 MASTER_NAME = "Listados maestro EMBALAJES de camiones.xlsx"
